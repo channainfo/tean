bcd117b00e28a319d36657c69a78b1c40d46be72
Kakada Chheang <kakada.chheang@gmail.com>
2014-11-11 17:07:45 +0700

    create post api

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -3,5 +3,19 @@ module Api::V1
     def index
       render json: User.all
     end
+
+    def create
+      post = Post.new(protected_params)
+      if post.save
+        head 201
+      else
+        head 422
+      end
+    end
+
+    private
+    def protected_params
+      params.permit(:title, :description)
+    end
   end
 end
--- spec/acceptance/post_spec.rb
+++ spec/acceptance/post_spec.rb
@@ -2,11 +2,23 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 resource "Posts" do
+  before(:each) do
+    header "Authorization", header_token
+  end
+
   get '/api/v1/posts' do
     example 'Listing posts' do
       do_request
       expect(status).to eq 200
     end
+  end
+
+  post '/api/v1/posts' do
+    example "Creating an post" do
+      do_request(title: "testing", description: "testing")
+      status.should == 201
+    end
+
   end
 
 end
